b2b83204f2fa09d88a656da82da7d040491a377e
ramzan <ramkurbanov10@yandex.ru>
2021-09-27 15:51:41 +0300

    Добавь первую функцию

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function getRandomNumber (min, max) {
+  if (min < 0 || min >= max) {
+    return false;
+  } else {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+}
+
+getRandomNumber();
