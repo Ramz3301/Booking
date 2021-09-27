9b69eb2f42acc10881febeabb5127123e455367d
ramzan <ramkurbanov10@yandex.ru>
2021-09-27 20:11:31 +0300

    Удали лишние else

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 function getRandomNumber (min, max) {
   if (min < 0 || min >= max) {
     return false;
-  } else {
-    return Math.floor(Math.random() * (max - min + 1) + min);
   }
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 getRandomNumber();
@@ -11,10 +10,9 @@ getRandomNumber();
 function getFloatingNumber (min, max, decimalPoint) {
   if (min < 0 || min >= max) {
     return false;
-  } else {
-    const randomNumber = Math.random() * (max - min + 1) + min;
-    return randomNumber.toFixed(decimalPoint);
   }
+  const randomNumber = Math.random() * (max - min + 1) + min;
+  return randomNumber.toFixed(decimalPoint);
 }
 
 getFloatingNumber();
