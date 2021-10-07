a94d489a34f551ac38f1150783104bc683a6926f
ramzan <ramkurbanov10@yandex.ru>
2021-10-07 08:45:01 +0300

    Добавь широту и долготу в константу

--- js/main.js
+++ js/main.js
@@ -18,11 +18,6 @@ const AUTHOR = {
   avatar: `img/avatars/user${  getRandomPositiveInteger(1, 10)  }.png`,
 };
 
-const locationAddress = {
-  lat: getRandomPositiveFloat(35.65, 35.7, 5),
-  lng: getRandomPositiveFloat(139.7, 139.8, 5),
-};
-
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 const SIMILAR_ADS_COUNT = 10;
@@ -31,11 +26,25 @@ const CHECKOUT = ['12:00', '13:00', '14:00'];
 const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const LATITUDE = {
+  min: 35.65,
+  max: 35.7,
+  decimals: 5,
+};
+const LONGITUDE = {
+  min: 139.7,
+  max: 139.8,
+  decimals: 5,
+};
 
+const locationAddress = {
+  lat: getRandomPositiveFloat(LATITUDE.min,LATITUDE.max, LATITUDE.decimals),
+  lng: getRandomPositiveFloat(LONGITUDE.min, LONGITUDE.max, LONGITUDE.decimals),
+};
 // https://qastack.ru/programming/2450954/how-to-randomize-shuffle-a-javascript-array Функция взята отсюда
 
 function shuffle(elements) {
-  let currentIndex = elements.length, temporaryValue, randomIndex;
+  let currentIndex = elements.length, randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
@@ -45,9 +54,10 @@ function shuffle(elements) {
     currentIndex -= 1;
 
     // And swap it with the current element.
-    temporaryValue = elements[currentIndex];
-    elements[currentIndex] = elements[randomIndex];
-    elements[randomIndex] = temporaryValue;
+    // temporaryValue = elements[currentIndex];
+    // elements[currentIndex] = elements[randomIndex];
+    // elements[randomIndex] = temporaryValue;
+    [elements[currentIndex], elements[randomIndex]] = [elements[randomIndex], elements[currentIndex]];
   }
 
   return elements.slice(getRandomArrayElement(0, elements.length - 1), elements.length);
@@ -55,7 +65,7 @@ function shuffle(elements) {
 
 const offer = {
   title: 'Тип жилья',
-  address: 'locationAddress.lat + locationAddress.lng',
+  address: `${locationAddress.lat}, ${locationAddress.lng}`,
   price: getRandomPositiveInteger(70000, 200000),
   type: getRandomArrayElement(TYPES),
   rooms: getRandomPositiveInteger(1, 8),
