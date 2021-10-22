058798d829e6b0c52e28b0ec870e54958ed5b113
ramzan <ramkurbanov10@yandex.ru>
2021-10-22 17:07:33 +0300

    Измени диапазон

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ import { getSimilarAdverts } from './data.js';
 import { getRandomPositiveInteger } from './utils.js';
 
 const similarAdverts = getSimilarAdverts();
-createSimilarAdvert(similarAdverts[getRandomPositiveInteger(1,10)]);
+createSimilarAdvert(similarAdverts[getRandomPositiveInteger(0,9)]);
