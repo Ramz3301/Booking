1cb5523d43b832890197c33323e368465f55b417
ramzan <ramkurbanov10@yandex.ru>
2021-10-25 19:45:26 +0300

    Добавь максимальное число для цены

--- js/form.js
+++ js/form.js
@@ -1,29 +1,68 @@
-const MIN_NAME_LENGTH = 30;
-const MAX_NAME_LENGTH = 100;
-const advertInputTitle = document.querySelector('#title');
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const form = document.querySelector('.ad-form');
+const inputAdvertTitle = form.querySelector('#title');
+const roomSelect = form.querySelector('#room-number');
+const capacitySelect = form.querySelector('#capacity');
+const price = form.querySelector('#price');
+const typeOfHousing = form.querySelector('#type');
+const TYPE_MIN_PRICE = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
 
-// advertInputTitle.addEventListener('invalid', () => {
-//   if (advertInputTitle.validity.tooShort) {
-//     advertInputTitle.setCustomValidity('Объявление должно состоять минимум из 30 симв.');
-//   } else if (advertInputTitle.validity.tooLong) {
-//     advertInputTitle.setCustomValidity('Объявление не должно превышать 100 симв.');
-//   } else if (advertInputTitle.validity.valueMissing) {
-//     advertInputTitle.setCustomValidity('Обязательное поле');
-//   } else {
-//     advertInputTitle.setCustomValidity('');
-//   }
-// });
-
-// advertInputTitle();
-
-advertInputTitle.addEventListener('input', () => {
-  const valueLength = advertInputTitle.value.length;
-  if (valueLength < MIN_NAME_LENGTH) {
-    advertInputTitle.setCustomValidity(`Ещё ${ MIN_NAME_LENGTH - valueLength } симв.`);
-  } else if (valueLength > MAX_NAME_LENGTH) {
-    advertInputTitle.setCustomValidity(`Удалите лишние ${ valueLength - MAX_NAME_LENGTH } симв.`);
+inputAdvertTitle.addEventListener('input', () => {
+  const valueLength = inputAdvertTitle.value.length;
+  if (valueLength < MIN_TITLE_LENGTH) {
+    inputAdvertTitle.setCustomValidity(`Ещё ${ MIN_TITLE_LENGTH - valueLength } симв.`);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    inputAdvertTitle.setCustomValidity(`Удалите лишние ${ valueLength - MAX_TITLE_LENGTH } симв.`);
   } else {
-    advertInputTitle.setCustomValidity('');
+    inputAdvertTitle.setCustomValidity('');
   }
-  advertInputTitle.reportValidity();
+  inputAdvertTitle.reportValidity();
 });
+
+function checkMaxPrice() {
+  price.addEventListener('invalid', () => {
+    if (price.value > 1000000) {
+      price.setCustomValidity('Максимальное значение - 1000000');
+    } else {
+      price.setCustomValidity('');
+    }
+  });
+}
+
+function checkRoomsCapacity () {
+  const capacityOption = capacitySelect.querySelectorAll('option');
+  roomSelect.addEventListener('change', function () {
+    const valueNumber = this.value;
+    if (valueNumber === '1') {
+      capacityOption[0].style.display = 'none';
+      capacityOption[1].style.display = 'none';
+      capacityOption[2].style.display = 'inline';
+      capacityOption[3].style.display = 'none';
+    } else if (valueNumber === '2') {
+      capacityOption[0].style.display = 'none';
+      capacityOption[1].style.display = 'inline';
+      capacityOption[2].style.display = 'inline';
+      capacityOption[3].style.display = 'none';
+    } else if (valueNumber === '3') {
+      capacityOption[0].style.display = 'inline';
+      capacityOption[1].style.display = 'inline';
+      capacityOption[2].style.display = 'inline';
+      capacityOption[3].style.display = 'none';
+    } else if (valueNumber === '100') {
+      capacityOption[0].style.display = 'none';
+      capacityOption[1].style.display = 'none';
+      capacityOption[2].style.display = 'none';
+      capacityOption[3].style.display = 'inline';
+    }
+  });
+}
+
+export {checkMaxPrice};
+export {checkRoomsCapacity};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 import { createSimilarAdvert } from './popup.js';
 import { getSimilarAdverts } from './data.js';
 import { getRandomPositiveInteger } from './utils.js';
+import { checkMaxPrice } from './form.js';
+import { checkRoomsCapacity } from './form.js';
 
 const similarAdverts = getSimilarAdverts();
 createSimilarAdvert(similarAdverts[getRandomPositiveInteger(0,9)]);
+checkMaxPrice();
+checkRoomsCapacity();
