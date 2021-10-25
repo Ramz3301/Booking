72e665ceb06778d10ac2460e89a22249a9b307ef
ramzan <ramkurbanov10@yandex.ru>
2021-10-25 19:52:17 +0300

    Закомментируй неиспользованные переменные

--- js/form.js
+++ js/form.js
@@ -5,14 +5,14 @@ const inputAdvertTitle = form.querySelector('#title');
 const roomSelect = form.querySelector('#room-number');
 const capacitySelect = form.querySelector('#capacity');
 const price = form.querySelector('#price');
-const typeOfHousing = form.querySelector('#type');
-const TYPE_MIN_PRICE = {
-  bungalow: 0,
-  flat: 1000,
-  hotel: 3000,
-  house: 5000,
-  palace: 10000,
-};
+// const typeOfHousing = form.querySelector('#type');
+// const TYPE_MIN_PRICE = {
+//   bungalow: 0,
+//   flat: 1000,
+//   hotel: 3000,
+//   house: 5000,
+//   palace: 10000,
+// };
 
 inputAdvertTitle.addEventListener('input', () => {
   const valueLength = inputAdvertTitle.value.length;
@@ -41,14 +41,14 @@ function checkRoomsCapacity () {
   roomSelect.addEventListener('change', function () {
     const valueNumber = this.value;
     if (valueNumber === '1') {
-      capacityOption[0].style.display = 'none';
+      capacityOption[0].style.display = 'inline';
       capacityOption[1].style.display = 'none';
-      capacityOption[2].style.display = 'inline';
+      capacityOption[2].style.display = 'none';
       capacityOption[3].style.display = 'none';
     } else if (valueNumber === '2') {
-      capacityOption[0].style.display = 'none';
+      capacityOption[0].style.display = 'inline';
       capacityOption[1].style.display = 'inline';
-      capacityOption[2].style.display = 'inline';
+      capacityOption[2].style.display = 'none';
       capacityOption[3].style.display = 'none';
     } else if (valueNumber === '3') {
       capacityOption[0].style.display = 'inline';
