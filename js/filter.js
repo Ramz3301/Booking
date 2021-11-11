a860721574fdbca96abe07394958cf6bbd29fedd
ramzan <ramkurbanov10@yandex.ru>
2021-11-11 15:28:28 +0300

    Добавь фильтрацию для количества гостей

--- js/filter.js
+++ js/filter.js
@@ -5,11 +5,13 @@ const DEFAULT_VALUE = 'any';
 const mapFilters = document.querySelector('.map__filters');
 const housingTypeSelect = mapFilters.querySelector('#housing-type');
 const roomsSelect = mapFilters.querySelector('#housing-rooms');
+const guestsSelect = mapFilters.querySelector('#housing-guests');
 
 const selectedType = (advert) => housingTypeSelect.value === advert.offer.type || housingTypeSelect.value  === DEFAULT_VALUE;
-const selectedRooms = (advert) => +roomsSelect.value === advert.offer.rooms || +roomsSelect.value === DEFAULT_VALUE;
+const selectedRooms = (advert) => +roomsSelect.value === advert.offer.rooms || roomsSelect.value === DEFAULT_VALUE;
+const selectedGuests = (advert) => +guestsSelect.value === advert.offer.guests || guestsSelect.value === DEFAULT_VALUE;
 
-const filterAdverts = (adverts) => adverts.filter((advert) => selectedType(advert) && selectedRooms(advert));
+const filterAdverts = (adverts) => adverts.filter((advert) => selectedType(advert) && selectedRooms(advert) && selectedGuests(advert));
 
 const createFilteredAdverts = (adverts) => {
   const filteredAdverts = filterAdverts(adverts);
