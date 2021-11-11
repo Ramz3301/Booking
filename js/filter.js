c11048b51185b6227e0ef1948b94b8714221f1ff
ramzan <ramkurbanov10@yandex.ru>
2021-11-11 15:21:46 +0300

    Добавь фильтрацию комнат

--- js/filter.js
+++ js/filter.js
@@ -4,11 +4,12 @@ import { debounce } from './utils/debounce.js';
 const DEFAULT_VALUE = 'any';
 const mapFilters = document.querySelector('.map__filters');
 const housingTypeSelect = mapFilters.querySelector('#housing-type');
-
+const roomsSelect = mapFilters.querySelector('#housing-rooms');
 
 const selectedType = (advert) => housingTypeSelect.value === advert.offer.type || housingTypeSelect.value  === DEFAULT_VALUE;
+const selectedRooms = (advert) => +roomsSelect.value === advert.offer.rooms || +roomsSelect.value === DEFAULT_VALUE;
 
-const filterAdverts = (adverts) => adverts.filter((advert) => selectedType(advert));
+const filterAdverts = (adverts) => adverts.filter((advert) => selectedType(advert) && selectedRooms(advert));
 
 const createFilteredAdverts = (adverts) => {
   const filteredAdverts = filterAdverts(adverts);
